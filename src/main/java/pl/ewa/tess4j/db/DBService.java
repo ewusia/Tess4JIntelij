e062a8f478b69be9618319c6d5829c2c5429362a
Adam Ambrożek <adam.ambrozek@gmail.com>
2019-05-05 22:54:46 +0200

    Poprawki w DB

--- src/main/java/pl/ewa/tess4j/db/DBService.java
+++ src/main/java/pl/ewa/tess4j/db/DBService.java
@@ -1,66 +1,68 @@
 package pl.ewa.tess4j.db;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 
 public class DBService {
 
-    static File dbLocation = new File(System.getProperty("user.home")+File.separator+"db.json");
+    static File dbLocation = new File(System.getProperty("user.home") + File.separator + "db.json");
     static ObjectMapper mapper = new ObjectMapper();
     static DB db;
 
-    public static DB getDb(){
-        if(db == null) {
+    static {
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static DB getDb() {
+        if (db == null) {
             try {
                 db = mapper.readValue(dbLocation, DB.class);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new DBException(e);
             }
-        }return db;
-    }
 
-    public static void saveDB(){
-        try {
-            mapper.writeValue(dbLocation,db);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            //to wywoła zapisanie bazy przed wylaczeniem aplikacji
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> saveDB()));
         }
+        return db;
     }
 
-    @Override
-    protected void finalize() throws Throwable {
-        saveDB();
+    public static void saveDB() {
+        try {
+            mapper.writeValue(dbLocation, db);
+        } catch (IOException e) {
+            throw new DBException(e);
+        }
     }
 
     public static void main(String[] args) throws Exception {
 
         DB db = getDb();
         db.getKategorie().get(0).setNazwa("huj");
+
+//        Sklep auchan = new Sklep("Auhan");
+//        Produkt laciate = new Produkt("Laciate", 4.0f, Arrays.asList(auchan));
+//        Produkt mars = new Produkt("Mars", 1.0f, Arrays.asList(auchan));
+//
+//        Kategoria slodyczne = new Kategoria("Slodycze", Arrays.asList(mars));
+//        Kategoria nabial = new Kategoria("Nabial", Arrays.asList(laciate));
+//
+//
+//        DB db = new DB(Arrays.asList(slodyczne,nabial));
+//
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        mapper.writeValue(dbLocation, db);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
         saveDB();
-
-        /*Sklep auchan = new Sklep("Auhan");
-        Produkt laciate = new Produkt("Laciate", 4.0f, Arrays.asList(auchan));
-        Produkt mars = new Produkt("Mars", 1.0f, Arrays.asList(auchan));
-
-        Kategoria slodyczne = new Kategoria("Slodycze", Arrays.asList(mars));
-        Kategoria nabial = new Kategoria("Nabial", Arrays.asList(laciate));
-
-
-        DB db = new DB(Arrays.asList(slodyczne,nabial));
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        mapper.writeValue(dbLocation, db);*/
     }
 
 
